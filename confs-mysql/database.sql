ae12394e18c5493a924c3f08161165190b75b821
João Preto <124364195+JpRevolut@users.noreply.github.com>
2026-02-11 15:27:09 -0300

    Update database.sql

--- confs-mysql/database.sql
+++ confs-mysql/database.sql
@@ -1,17 +1,20 @@
+CREATE DATABASE vida_dos_bichos;
+CREATE USER 'vidabichos'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON vida_dos_bichos.* TO 'vidabichos'@'%';
+FLUSH PRIVILEGES;
+EXIT;
 
-CREATE DATABASE controletarefas;
-USE controletarefas;
-
-CREATE TABLE usuarios (
+CREATE TABLE categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    nome VARCHAR(100)
 );
 
-CREATE TABLE tarefas (
+
+CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    usuario_id INT,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    nome VARCHAR(100),
+    preco DECIMAL(10,2),
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
+
