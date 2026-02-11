0a6522cc069b66564e20b6d393d24c2f329d4258
João Preto <124364195+JpRevolut@users.noreply.github.com>
2026-02-11 15:29:33 -0300

    Update index.php

--- site-backend/index.php
+++ site-backend/index.php
@@ -1,15 +1,19 @@
 
+<?php
+include 'conexao.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-<title>Backend</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Vida dos Bichos - Administração</title>
 </head>
-<body class="container mt-5">
+<body>
+
 <h1>Área Administrativa</h1>
-<ul>
-<li><a href="usuarios.php">Gerenciar Usuários</a></li>
-<li><a href="tarefas.php">Gerenciar Tarefas</a></li>
-</ul>
+
+<p>Conexão com o banco realizada com sucesso.</p>
+
 </body>
 </html>
